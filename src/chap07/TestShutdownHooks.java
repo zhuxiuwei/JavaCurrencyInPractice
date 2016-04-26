23069dca1007168d2ad3b48a30d6ef4ce245c093
Xiuwei Zhu <jlu_zxw@126.com>
2016-04-26 16:59:15 +0800

    add isInterrupted test in TestShutdownHooks

--- src/chap07/TestShutdownHooks.java
+++ src/chap07/TestShutdownHooks.java
@@ -26,23 +26,34 @@ public class TestShutdownHooks implements Runnable{
 			try {
 				Thread.sleep(500);
 			} catch (InterruptedException e) {	//responsive to interrupt
-				System.out.println("AwsClient Interrupt!");
+				System.out.println("TestShutdownHooks Interrupted!");
 				Thread.currentThread().interrupt();		//restore interruption state
 			}
 		}
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		Thread t = new Thread(new TestShutdownHooks());
 		t.start();
 		
 		try {
-			Thread.sleep(3000);
+			Thread.sleep(2000);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 		//System.exit(0);	//!!!!This will shutdown JVM, then trigger ShutdownHook.!!!!!!!!!!!!
 		t.interrupt();	////!!!!This can also shutdown JVM, as AutoFlush is a daemon thread. then trigger ShutdownHook!!!!!!!!!!!!
+		
+		System.out.println("\r\n******* Test isInterrupted again! *******");
+		/**测试isInterrupted为啥总返回false。这个帖子一句话提醒了我："@rAy except your thread immediately dies. – Peter Lawrey Dec 19 '13 at 9:47" http://stackoverflow.com/questions/20677604/thread-isinterrupted-always-returns-false 
+		        所以，在Thread isAlive的时候，isInterrupted能正确返回true。但是Thread dies后，看起来interrupt flag又被reset了。 */
+		boolean isInterrupt = t.isInterrupted();
+		boolean isAlive = t.isAlive();
+		/** 因为在interrupt()后的第一时间保存好isAlive和isInterrupt，此时在极短的瞬间t还是alive的，所以返回true true */
+		System.out.println("isInterrupt: " + isInterrupt + ", isAlive: " + isAlive);	//true true
+		/** 貌似调用System.out.println会耗费一些时间，此时Thread已经die了，所以打印false false */
+		Thread.sleep(5);
+		System.out.println("isInterrupt: " + t.isInterrupted() + ", isAlive: " + t.isAlive());	//false false
 	}
 }
 
